7b13b61b00319c5f31490e102b925f590b711930
aaronMeche <70254600+aaron-meche@users.noreply.github.com>
2022-07-19 17:18:06 -0500

    os updates

--- os/main.js
+++ os/main.js
@@ -1,12 +1,31 @@
-function bodyScroll() {
-    var scroll = document.getElementById('body').scrollTop;
-    console.log(scroll);
-    var wrapper = document.getElementById('screenContainer').style;
-    if (scroll < 100) {
-        wrapper.top = '0';
-    } else if ((scroll >= 100) && (scroll < 200)) {
-        wrapper.top = '-100vh';
-    } else if ((scroll >= 200) && (scroll < 300)) {
-        wrapper.top = '-200vh';
+function launch(applicationTitle) {
+    document.getElementById(applicationTitle + '.app').style.display = 'block';
+}
+
+var mouseIsDown = false;
+var selectedWindow;
+var idTimeout;
+
+var appTitles = document.querySelector('.title');
+
+function titlePress(self) {
+    mouseIsDown = true;
+    selectedWindow = self;
+    idTimeout = setTimeout(function() {
+        if(mouseIsDown) {
+            console.log('2 seconds')
+        }
+    }, 2000);
+}
+
+function titleUnpress(self) {
+    clearTimeout(idTimeout);
+    mouseIsDown = false;
+}
+
+function mouseMoved() {
+    if (mouseIsDown) {
+        console.log('moving window');
+        self.style.top = '100vh';
     }
 }
