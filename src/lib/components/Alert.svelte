8611dab240f90fc655391247e8991739be6a35d4
Aaron Meche <aaronmeche18@icloud.com>
2023-03-20 13:36:30 -0500

    Stable release

---
+++ src/lib/components/Alert.svelte
@@ -0,0 +1,22 @@
+<script>
+    // export let title
+</script>
+
+<!--  -->
+
+<div class="card">
+    Hello
+</div>
+
+<!--  -->
+
+<style>
+	.card{
+        height: 50vh;
+        width: 50vw;
+        position: fixed;
+        top: 25vh;
+        left: 25vw;
+        background: black;
+	}
+</style>
