f91341f25b226fecd462bf10a70de4d855c85a54
Aaron Meche <aaronmeche18@icloud.com>
2023-11-15 23:32:14 -0600

    trying new deploy

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,9 @@ export default {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'docs',
-			assets: 'docs',
-			fallback: undefined,
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
 			precompress: false,
 			strict: true
 		})
