5b90d80d9f2eed0e81d0a7d304b196f1e1e2ac61
Aaron Meche <aaronmeche18@icloud.com>
2023-11-15 23:45:01 -0600

    New adapter and deploy code

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+// svelte.config.js
+import adapter from 'svelte-adapter-github';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+export default {
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'docs',
+      assets: 'docs',
+      fallback: null,
+      precompress: false,
+      domain: '',
+      jekyll: false
+    })
+  }
 };
-
-export default config;
